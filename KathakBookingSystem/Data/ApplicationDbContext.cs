a7bedbf1bcc86212453bb8e7606e2a47438d399d
Iamneo <techadmin@iamneo.ai>
2023-09-18 16:58:12 +0000

    Backup Commit

--- KathakBookingSystem/Data/ApplicationDbContext.cs
+++ KathakBookingSystem/Data/ApplicationDbContext.cs
@@ -13,7 +13,14 @@ namespace KathakBookingSystem.Data
 
         public virtual DbSet<Class> Classes { get; set; }
         public virtual DbSet<Student> Students { get; set; }
-
+protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("User ID=sa;password=examlyMssql@123;server=localhost;Database=KathakBookingDB;trusted_connection=false;Persist Security Info=false;Encrypt=false");
+            }
+        }
         // Write your ApplicationDbContext here...
     }
 }
