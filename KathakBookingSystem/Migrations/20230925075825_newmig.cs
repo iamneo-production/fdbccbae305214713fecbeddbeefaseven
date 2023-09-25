9a60927a90505ccb2bec36850d53f66dd5b469ca
Iamneo <techadmin@iamneo.ai>
2023-09-25 08:04:38 +0000

    Backup Commit

---
+++ KathakBookingSystem/Migrations/20230925075825_newmig.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace KathakBookingSystem.Migrations
+{
+    public partial class newmig : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Student_Class_ClassID",
+                table: "Student");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Student",
+                table: "Student");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Class",
+                table: "Class");
+
+            migrationBuilder.RenameTable(
+                name: "Student",
+                newName: "Students");
+
+            migrationBuilder.RenameTable(
+                name: "Class",
+                newName: "Classes");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Student_ClassID",
+                table: "Students",
+                newName: "IX_Students_ClassID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Students",
+                table: "Students",
+                column: "StudentID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Classes",
+                table: "Classes",
+                column: "ClassID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Students_Classes_ClassID",
+                table: "Students",
+                column: "ClassID",
+                principalTable: "Classes",
+                principalColumn: "ClassID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Students_Classes_ClassID",
+                table: "Students");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Students",
+                table: "Students");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Classes",
+                table: "Classes");
+
+            migrationBuilder.RenameTable(
+                name: "Students",
+                newName: "Student");
+
+            migrationBuilder.RenameTable(
+                name: "Classes",
+                newName: "Class");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Students_ClassID",
+                table: "Student",
+                newName: "IX_Student_ClassID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Student",
+                table: "Student",
+                column: "StudentID");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Class",
+                table: "Class",
+                column: "ClassID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Student_Class_ClassID",
+                table: "Student",
+                column: "ClassID",
+                principalTable: "Class",
+                principalColumn: "ClassID",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
