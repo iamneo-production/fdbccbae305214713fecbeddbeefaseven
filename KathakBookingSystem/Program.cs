7916ecf92c60f003a8e11648482edbf0d8429d29
Iamneo <techadmin@iamneo.ai>
2023-09-25 07:54:43 +0000

    Backup Commit

--- KathakBookingSystem/Program.cs
+++ KathakBookingSystem/Program.cs
@@ -28,6 +28,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Class}/{action=AvailableClasses}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
